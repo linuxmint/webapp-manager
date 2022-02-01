0ee7266350477760b78a51543733501c5cb087ee
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2022-01-31 20:32:49 -0800

    Another fix for firefox

--- usr/share/webapp-manager/firefox/profile/user.js
+++ usr/share/webapp-manager/firefox/profile/user.js
@@ -9,3 +9,4 @@ user_pref("browser.tabs.warnOnClose", false);
 user_pref("plugin.state.flash", 2);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.tabs.drawInTitlebar", false);
+user_pref("browser.tabs.inTitlebar", 0);
