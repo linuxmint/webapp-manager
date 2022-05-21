24eed9b5e2979b2fb0726a245c4ba7a60dda4a01
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2022-05-21 09:33:14 -0700

    Fixed Firefox SSBs with missing title bars (Fixes #132, #116, #91) (#146)
    Fixed Firefox SSBs with missing title bars (Fixes #132, #116, #91)

--- usr/share/webapp-manager/firefox/profile/user.js
+++ usr/share/webapp-manager/firefox/profile/user.js
@@ -8,5 +8,8 @@ user_pref("browser.ctrlTab.previews", true);
 user_pref("browser.tabs.warnOnClose", false);
 user_pref("plugin.state.flash", 2);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.tabs.drawInTitlebar", false);
+user_pref("browser.tabs.inTitlebar", 0);
 user_pref("browser.contentblocking.category", "strict");
 user_pref("network.cookie.lifetimePolicy", 0);
+
