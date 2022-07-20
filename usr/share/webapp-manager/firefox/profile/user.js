d42bfeb8db1de1e7998c60f497ff12f5d650397a
Brian Morison <777373+bkmo@users.noreply.github.com>
2022-07-19 23:55:25 -0600

    Update user.js

--- usr/share/webapp-manager/firefox/profile/user.js
+++ usr/share/webapp-manager/firefox/profile/user.js
@@ -12,4 +12,13 @@ user_pref("browser.tabs.drawInTitlebar", false);
 user_pref("browser.tabs.inTitlebar", 0);
 user_pref("browser.contentblocking.category", "strict");
 user_pref("network.cookie.lifetimePolicy", 0);
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("signon.rememberSignons", false);
+// Enable uBlock DarkReader and extension for browser: protocol by default
+user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.screenshots@mozilla.org", true);
+user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.uBlock0@raymondhill.net", true);
+user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.addon@darkreader.org", true);
+user_pref("extensions.getAddons.showPane", false); // [HIDDEN PREF]
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 
