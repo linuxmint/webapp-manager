d9fc76670d5f88b679cb4413f78592e7f909f1ce
Rokosun <79040025+futureisfoss@users.noreply.github.com>
2023-06-19 13:21:26 +0000

    Don't clear cookies in LibreWolf (#250)
    * Don't clear cookies in LibreWolf
    
    It seems like the user_pref to preserve cookies have changed.
    
    * Revert back the prefs changed in librewolf to clear data on exit

--- usr/share/webapp-manager/firefox/profile/user.js
+++ usr/share/webapp-manager/firefox/profile/user.js
@@ -11,5 +11,6 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.tabs.drawInTitlebar", false);
 user_pref("browser.tabs.inTitlebar", 0);
 user_pref("browser.contentblocking.category", "strict");
-user_pref("network.cookie.lifetimePolicy", 0);
-
+user_pref("privacy.clearOnShutdown.offlineApps", false);
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("privacy.sanitize.timeSpan", 1);
